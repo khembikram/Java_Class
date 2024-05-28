2f63faac62e1b6396b2ebc1ada7e6ec98caa75ec
Sujan Rana <khembikram1@gmail.com>
2024-05-28 11:11:33 +0545

    frame update

---
+++ SimpleFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+/**
+ * SimpleFrame
+ */
+public class SimpleFrame extends JFrame{
+
+     private static final int DEFAULT_WIDTH = 500;
+     private static final int DEFAULT_HEIGHT = 400;
+
+     SimpleFrame(){
+        setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
+     }
+}
