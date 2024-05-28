6c0d2ef715454ff40f62b39948d87bd52194d73c
Sujan Rana <khembikram1@gmail.com>
2024-05-28 11:17:02 +0545

    more change

--- JFrameDemo.java
+++ JFrameDemo.java
@@ -4,6 +4,7 @@ public class JFrameDemo {
     public static void main (String[] args){
         SwingUtilities.invokeLater(()->{
             SimpleFrame frame = new SimpleFrame();
+            frame.setTitle("Demo Demo Demo");
             frame.setVisible(true);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         });
