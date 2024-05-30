6ac164508e51e17bd5fbdf5bd081cb00d4a29e83
Sujan Rana <khembikram1@gmail.com>
2024-05-30 10:44:53 +0545

    log for the setbounds

--- SimpleFrame.java
+++ SimpleFrame.java
@@ -9,6 +9,10 @@ public class SimpleFrame extends JFrame {
     private static final int DEFAULT_HEIGHT = 400;
 
     SimpleFrame() {
-        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        // setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        // setLocation(500,200);
+
+        // Using setBounds
+        setBounds(500, 200, DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
 }
