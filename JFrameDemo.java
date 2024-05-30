91acc2981359e2f94f343ede8296883fa90c4ab9
Sujan Rana <khembikram1@gmail.com>
2024-05-30 10:31:31 +0545

    flow Layout code

--- JFrameDemo.java
+++ JFrameDemo.java
@@ -1,21 +1,25 @@
 import javax.swing.SwingUtilities;
-
 import java.awt.Color;
 import java.awt.Font;
-
+import java.awt.FlowLayout;
 import javax.swing.*;
+
 public class JFrameDemo {
     public static void main (String[] args){
         SwingUtilities.invokeLater(()->{
             SimpleFrame frame = new SimpleFrame();
             frame.setTitle("Demo Demo Demo");
+
+            frame.getContentPane().setLayout(new FlowLayout());
+
             JButton button = new JButton("Click Me!");
-            button.setFont(new Font( "Arial", Font.BOLD,16));
+            button.setFont(new Font("Arial", Font.BOLD, 16));
             button.setBackground(Color.BLUE);
             button.setForeground(Color.WHITE);
-            button.addActionListener(e->{
+            button.addActionListener(e -> {
                 JOptionPane.showMessageDialog(frame, "Hi", "Message", JOptionPane.INFORMATION_MESSAGE);
             });
+
             frame.getContentPane().add(button);
             frame.setVisible(true);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
