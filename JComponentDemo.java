4e80927961e1313cb24b3f09251f168ad596ee74
Sujan Rana <khembikram1@gmail.com>
2024-05-31 17:56:46 +0545

    main update remain

--- JComponentDemo.java
+++ JComponentDemo.java
@@ -1,3 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+class ComponentDemo extends JComponent{
+    private static final int DEFAULT_WIDTH = 500;
+    private static final int DEFAULT_HEIGHT = 500;
+
+    public void pointComponent (Graphics g) {
+        g.drawString("Displaying Information in a component", 20, 20);
+    }
+    public Dimension getPreferredSize(){
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
+
+class FrameDemo extends JFrame{
+    public FrameDemo() {
+        add(new ComponentDemo());
+        pack();
+    }
+}
+
+
 public class JComponentDemo {
     
 }
