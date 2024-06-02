3fc2abfe5fde72fa589bab4d8f3e748b495b9720
Sujan Rana <khembikram1@gmail.com>
2024-06-02 22:46:46 +0545

    ellipse frame

--- EllipseComponent.java
+++ EllipseComponent.java
@@ -1,9 +1,47 @@
-import  javax.swing.*;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import javax.swing.*;
 
 class ComponentDemo extends JComponent {
-    
+
+    private static final int DEFAULT_WIDTH = 400;
+    private static final int DEFAULT_HEIGHT = 300;
+
+    protected void paintComponent(Graphics2D g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        double lefX = 50;
+        double topY = 50;
+        double width = 200;
+        double height = 150;
+
+        Ellipse2D ellipse = new Ellipse2D.Double(lefX, topY, width, height);
+        g2.draw(ellipse);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
+
+class FrameDemo extends JFrame {
+
+    public FrameDemo() {
+        add(new ComponentDemo());
+        pack();
+    }
 }
 
 public class EllipseComponent {
-    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()->{
+            FrameDemo frame = new FrameDemo();
+            frame.setTitle("Ellipse 2D Drawing");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
 }
